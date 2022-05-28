ade86f469e0245ef849ae1a922805b97368971d9
JoeRaig <raigjoe@gmail.com>
2022-05-28 17:26:36 +0200

    wip obelisk ui | flicker effect

--- Assets/_Scripts/Debugger.cs
+++ Assets/_Scripts/Debugger.cs
@@ -5,13 +5,16 @@ public class Debugger : MonoBehaviour
     [SerializeField] GameObject enemyPrefab1;
     [SerializeField] GameObject enemyPrefab2;
     [SerializeField] GameObject enemyPrefab3;
+    [SerializeField] GameObject minionPrefab;
     [SerializeField] Transform enemyPool;
+    [SerializeField] Transform minionPool;
 
     void Update()
     {
         InvokeThing1();
         InvokeThing2();
         InvokeThing3();
+        InvokeThing4();
     }
 
     void InvokeThing1()
@@ -35,6 +38,14 @@ public class Debugger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             Instantiate(enemyPrefab3, transform.position, Quaternion.identity, enemyPool);
+        }
+    }
+
+    void InvokeThing4()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Instantiate(minionPrefab, transform.position, Quaternion.identity, minionPool);
         }
     }
 }
--- Assets/_Scripts/Player/ObeliskHealth.cs
+++ Assets/_Scripts/Player/ObeliskHealth.cs
@@ -1,26 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObeliskHealth : MonoBehaviour
 {
+    [SerializeField] Image fillArea;
+    Slider obeliskHealthBar;
+
     int initialHitPoints = 1000;
     
     int currentHitPoints = 0;
     public int CurrentHitPoints { get => currentHitPoints; set => currentHitPoints = value; }
 
+    Color initialcolor;
+
+    void Awake()
+    {
+        obeliskHealthBar = GameObject.Find("ObeliskHealthBar").GetComponent<Slider>();
+    }
+
     void Start()
     {
         currentHitPoints = initialHitPoints;
+        initialcolor = fillArea.color;
+
+        DisplaySlider();
     }
 
     void Update()
     {
-        Debug.Log(currentHitPoints);
+        DisplaySlider();
+    }
 
-        if (currentHitPoints <= 0)
-        {
-            Debug.Log("OBELISK DESTROY!!");
-        }
+    void DisplaySlider()
+    {
+        obeliskHealthBar.value = currentHitPoints * 0.001f;
+    }
+
+    void FlickerEffect()
+    {
+        fillArea.color = Color.red;
     }
 }
