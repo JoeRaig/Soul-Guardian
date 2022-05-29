3d4052e3f7cf7530f805577fa496db7d65120835
JoeRaig <raigjoe@gmail.com>
2022-05-29 11:08:46 +0200

    Create music / sfx sample script objects | import audio test assets

---
+++ Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        SingletonMe();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySound(AudioClip audioClip, float volume = 1)
+    {
+        if (audioSource.enabled == false) { return; }
+
+        StopAudio();
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        if (audioSource.enabled == false) { return; }
+
+        audioSource.Stop();
+    }
+
+    public void PlayOneShot(AudioClip audioClip, float volume = 1)
+    {
+        if (audioSource.enabled == false) { return; }
+
+        audioSource.PlayOneShot(audioClip, volume);
+    }
+
+    void SingletonMe()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/SFXManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance;
+
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        SingletonMe();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySound(AudioClip audioClip, float volume = 1)
+    {
+        if (audioSource.enabled == false) { return; }
+
+        StopAudio();
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        if (audioSource.enabled == false) { return; }
+
+        audioSource.Stop();
+    }
+
+    public void PlayOneShot(AudioClip audioClip, float volume = 1)
+    {
+        if (audioSource.enabled == false) { return; }
+
+        audioSource.PlayOneShot(audioClip, volume);
+    }
+
+    void SingletonMe()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
